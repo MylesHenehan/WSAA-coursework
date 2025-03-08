b261826b9156231973eefb02566e9579f8e7ad97
MylesHenehan <mylesjhenehan98@gmail.com>
2025-03-08 16:52:27 +0000

    attempt 2 at writing some code

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -1,20 +1,24 @@
 import requests
+import base64
+import json
 from github import Github
 from config import config as cfg
 
 apikey = cfg["githubkey"]
+githuburl = "https://api.github.com/repos/MylesHenehan/privaterepo/contents/andrew.txt"
+replacementname = input("Please enter your name: ")
+
 g = Github(apikey)
 
-for repo in g.get_user().get_repos():
- print(repo.name)
+response = requests.get(githuburl, auth=('token', apikey))
+content = response.json()
 
-repo = g.get_repo("MylesHenehan/privaterepo")
-print(repo.clone_url)
+with open(content, "wb") as file:
+    file.replace("Andrew",replacementname)
 
-fileInfo = repo.get_contents("andrew.txt")
-urlOfFile = fileInfo.download_url
-print (urlOfFile)
 
-response = requests.get(urlOfFile)
-filecontent = response.text
-print(filecontent)
+
+
+
+
+
