2843014383b5dcfcec1a25db0456fe22b2ac179a
MylesHenehan <mylesjhenehan98@gmail.com>
2025-04-16 21:41:46 +0100

    fixing issues with code and adding comments

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -1,21 +1,22 @@
-import requests
-from github import Github
-from config import config as cfg
+import requests #needed to make HTTP requests
+from github import Github #allows me to interact with the Github API
+from config import config as cfg #my configuration file containing my github key
 
 apikey = cfg['githubkey']
-g = Github(apikey)
+g = Github(apikey) # in these 2 lines, I'm retrieving my key from the config file and authenticating myself
 
-repo = g.get_repo("MylesHenehan/privaterepo")
+repo = g.get_repo("MylesHenehan/privaterepo") # here, now I've authenticated myself, I can access my private repo
 
 fileinfo = repo.get_contents("andrew.txt")
+# print(fileinfo) - used to check the info about the file, including commit history
 fileurl = fileinfo.download_url
 
-response = requests.get(fileurl)
-filecontent = response
+response = requests.get(fileurl) # the get request retrieves the raw content from the file, while .text converts this to a string.
+filecontent = response.text
 print(filecontent)
 
-newcontent = filecontent.replace("Andrew", input("Please enter your name: "))
-gitHubResponse=repo.update_file(fileinfo.path,"updated by prog",newcontent,fileinfo.sha)
+newcontent = filecontent.replace("Andrew", input("Please enter your name: ")) # now I can replace the instances of "Andrew" in the file with whatever the user's name is.
+gitHubResponse=repo.update_file(fileinfo.path,"updated by prog",newcontent,fileinfo.sha) # finally, I commit this to Github
 print(gitHubResponse)
 
 
